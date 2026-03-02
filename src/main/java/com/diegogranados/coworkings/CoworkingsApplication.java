3042b24c47f00df44d7c42778fdd15ae160e97bb
DIEGUS15 <diegogranados1501@gmail.com>
2026-03-02 16:34:28 -0500

    Configuración inicial del proyecto

---
+++ src/main/java/com/diegogranados/coworkings/CoworkingsApplication.java
@@ -0,0 +1,13 @@
+package com.diegogranados.coworkings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoworkingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoworkingsApplication.class, args);
+	}
+
+}
