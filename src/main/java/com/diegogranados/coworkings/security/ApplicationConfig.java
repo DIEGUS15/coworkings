59fc50061e0fdd20799942678f20755c6a523994
DIEGUS15 <diegogranados1501@gmail.com>
2026-03-02 22:59:38 -0500

    fix: update DaoAuthenticationProvider to non-deprecated Spring Security

--- src/main/java/com/diegogranados/coworkings/security/ApplicationConfig.java
+++ src/main/java/com/diegogranados/coworkings/security/ApplicationConfig.java
@@ -28,8 +28,8 @@ public class ApplicationConfig {
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(passwordEncoder());
-        provider.setUserDetailsService(userDetailsService());
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
         return provider;
     }
 
